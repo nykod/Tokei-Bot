a1ed01d49a9f1b154920fc23093de257bc7cafe1
nykod <66205592+nykod@users.noreply.github.com>
2024-01-13 01:47:34 +0100

    Update main.py
    Removed token due to repository privacy

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ from other.bot import DiscordClient
 if __name__ == '__main__':
     client = DiscordClient.client
     DiscordClient().botEvents()
-    client.run('OTQ1MzA5MjIwMjg1MzIxMjY3.YhOR0Q.0q5UEaMkuj0-feQZbzCKOUSE89E')
+    client.run('') #insert token here
